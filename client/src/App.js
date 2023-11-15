2539e3c61537c425f4a34ecd140bdb872228f987
Ishrak Adit <ishrak.adit07@gmail.com>
2023-11-15 19:46:25 +0600

    First Commit for Frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+//Importing components
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <Fragment>
+        <h1>Social Media Client Side is Running</h1>
+      </Fragment>
+    </div>
+  );
+}
