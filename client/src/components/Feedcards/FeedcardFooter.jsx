b06685a4514dbb13f8e847585d5cda19a6371a98
Ishrak Adit <ishrak.adit07@gmail.com>
2023-11-29 15:39:32 +0600

    Commit for basic live like update

--- client/src/components/Feedcards/FeedcardFooter.jsx
+++ client/src/components/Feedcards/FeedcardFooter.jsx
@@ -4,10 +4,28 @@ import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlin
 
 export default function FeedcardFooter() {
 
+  const [postID, setPostID] = useState(" ");
   const [liked, setLiked] = useState(false);
+  const [postLikers, setPostLikers] = useState([]);
 
   const toggleLike = () =>{
     setLiked(!liked);
+  }
+
+  const getLikeUpdate = async() =>{
+    try {
+      
+      const getLikeUpdateQuery = `http://localhost:4000/posts/likers/${postID}`;
+      const response = await fetch(getLikeUpdateQuery);
+
+      if(response.status === 200){
+        const responseData = await response.json()
+        console.log(responseData);
+      }
+
+    } catch (err) {
+      console.error(err.message)
+    }
   }
   
   return (
